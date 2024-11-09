6b45b91a9d4a2d5980770c6f34d434d71bd52bc5
ahmeddsakrr <91761423+ahmeddsakrr@users.noreply.github.com>
2024-11-09 20:33:02 +0200

    Implemented Search Sessions

--- src/main/java/com/asu/EduMentor/controller/rest/SearchController.java
+++ src/main/java/com/asu/EduMentor/controller/rest/SearchController.java
@@ -1,9 +1,13 @@
 package com.asu.EduMentor.controller.rest;
 
 
+import com.asu.EduMentor.model.Session;
 import com.asu.EduMentor.model.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -15,5 +19,11 @@ public class SearchController {
     public ResponseEntity<List<User>> searchUsers(@RequestParam("search") String search) {
         List<User> users = User.findUsersBySearchTerm(search);
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/sessions")
+    public ResponseEntity<List<Session>> searchSessions(@RequestParam("search") String search) {
+        List<Session> sessions = Session.findSessionsBySearchTerm(search);
+        return ResponseEntity.ok(sessions);
     }
 }
