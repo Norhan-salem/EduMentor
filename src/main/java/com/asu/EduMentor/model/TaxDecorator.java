fe2c87a0101ca7dfaf6a5f74eb878bbfea544229
Norhan-salem <norhansalem581@gmail.com>
2024-11-11 15:38:23 +0200

    Added Tax Decorator

---
+++ src/main/java/com/asu/EduMentor/model/TaxDecorator.java
@@ -0,0 +1,39 @@
+package com.asu.EduMentor.model;
+
+/**
+ * A decorator class for the Invoice that adds tax to the total invoice amount.
+ */
+public class TaxDecorator extends InvoiceDecorator {
+    private static final double TAX_RATE = 0.12;
+
+    /**
+     * Constructs a TaxDecorator with a specified base invoice and applies the hard-coded tax rate.
+     *
+     * @param invoice The base invoice being decorated.
+     */
+    public TaxDecorator(Invoice invoice) {
+        super(invoice);
+    }
+
+    /**
+     * Returns the total amount of the invoice including the applied tax.
+     *
+     * @return Total amount of the invoice with tax.
+     */
+    @Override
+    public double getTotal() {
+        double baseTotal = super.getTotal();
+        return baseTotal + (baseTotal * TAX_RATE);
+    }
+
+    /**
+     * Gets the tax rate applied to the invoice.
+     *
+     * @return The tax rate.
+     */
+    public static double getTaxRate() {
+        return TAX_RATE;
+    }
+}
+
+
