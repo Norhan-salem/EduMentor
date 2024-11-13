1a5d68e19c38ddfce7bd607de07394f09ddd2558
nadaWagdy <nawagdy332@gmail.com>
2024-11-13 18:20:54 +0200

    Implemented SortByTotalHours

---
+++ src/main/java/com/asu/EduMentor/controller/rest/SortByTotalHours.java
@@ -0,0 +1,15 @@
+package com.asu.EduMentor.controller.rest;
+
+import com.asu.EduMentor.model.Mentor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class SortByTotalHours implements ISortingStrategy{
+    @Override
+    public ArrayList<Mentor> sort(ArrayList<Mentor> mentors) {
+        Collections.sort(mentors, Comparator.comparingDouble(Mentor::getTotalHours).reversed());
+        return mentors;
+    }
+}
