edc39ac1e436b28ae9b9bda9182944fe1c0df122
nadaWagdy <nawagdy332@gmail.com>
2025-01-14 22:44:53 +0200

    Added EmailRecipientCollection

---
+++ src/main/java/com/asu/EduMentor/socialMediaNotifier/Iterator/EmailRecipientCollection.java
@@ -0,0 +1,18 @@
+package com.asu.EduMentor.socialMediaNotifier.Iterator;
+
+import java.util.ArrayList;
+
+public class EmailRecipientCollection implements Iterable{
+
+    ArrayList<EmailRecipient> recipients;
+
+    public EmailRecipientCollection(){
+        recipients = new ArrayList<EmailRecipient>(); // TODO: fetch them from database
+        recipients.add(new EmailRecipient("peter", "peterpeter", "peter@elasticsdp.com"));
+    }
+
+    @Override
+    public Iterator createIterator() {
+        return new EmailRecipientsIterator(this.recipients);
+    }
+}
